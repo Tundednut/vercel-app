7e45688eecf4d3392db468bc2d4af7f742ff51d8
Tundednut <147956429+Tundednut@users.noreply.github.com>
2023-11-04 10:20:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
  const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
-const run= () =>{
-alert("Hello world")
-}
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<button onclick="run()">RUN</button>');
+  res.end('<!DOCTYPE html>
+          <html>
+          <head>
+          </head>
+          <body>
+          <button onclick="run()">RUN</button>
+          <script>
+          const run = ()=> alert("hello world");
+          </script>
+          </body>
+          </html>');
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
